6760162e78034aad9a4153add7b92fa4e390c2b5
s43501 <s43501@bj.ac.th>
2025-11-12 11:06:03 +0700

    Add files via upload

---
+++ a1_platfrom_39/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1762919946,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/coin-sheet1.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
