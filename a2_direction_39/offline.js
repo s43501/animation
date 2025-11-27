f3390ca29f09e4394ebad9d1af06120e41453055
s43501 <s43501@bj.ac.th>
2025-11-27 15:44:56 +0700

    Add files via upload

---
+++ a2_direction_39/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1764233250,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bg1-sheet0.png",
+		"images/collideobject-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/playerarnimation-sheet0.png",
+		"images/playerarnimation-sheet1.png",
+		"images/player-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
