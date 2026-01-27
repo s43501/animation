64e4279d4369de426e84d52a2e2e79987be0ef78
s43501 <s43501@bj.ac.th>
2026-01-27 15:51:53 +0700

    Add files via upload

---
+++ a6_flappybird_39/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1769503842,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/bg2-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/pipe_bottom-sheet0.png",
+		"images/floor1.png",
+		"images/floor2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
